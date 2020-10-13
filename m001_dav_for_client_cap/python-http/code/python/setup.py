374b935a10492e006ed5dbb3f269f4f1ba88487c
thierry_otvl <thierry@beigbeder.info>
2020-10-13 20:36:01 +0200

    added python app structure

---
+++ m001_dav_for_client_cap/python-http/code/python/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='otvl_web',
+      description='python server for otvl_pm001',
+      url='https://otvl.org/',
+      author='thierry_otvl',
+      author_email='thierry@otvl.org',
+      license='GNU AFFERO GENERAL PUBLIC LICENSE',
+      packages=['otvl_pm001'],
+      requires=[]
+      )
---
+++ m001_dav_for_client_cap/python-http/code/shell/docker_cmd.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+/srv/venv/bin/python \
+    -m otvl_pm001.server
