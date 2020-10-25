f9f41b555d1114b9faaec9b4fdfddb3f2f98ab30
thierry_otvl <thierry@beigbeder.info>
2020-10-25 18:26:01 +0100

    updated configuration

--- m002_dav_with_shibb/apache-rp/build/gen_cert.sh
+++ m002_dav_with_shibb/apache-rp/build/gen_cert.sh
@@ -2,10 +2,10 @@ gen_cert() {
   fn=$1
   SVCN=$2
   SVDN="/C=FR/ST=Occitanie/L=Toulouse/O=otvl org/CN=${SVCN}"
-  openssl req -new -x509 -days 3650 -nodes -out $fn.crt -keyout $fn.key -subj "$SVDN"
+  openssl req -x509 -newkey rsa:4096 -nodes -keyout $fn.key -out $fn.crt -days 3650 -subj "$SVDN" -addext "subjectAltName = DNS:$SVCN"
   openssl x509 -noout -text -in $fn.crt | egrep "Subject:|Issuer:|Serial Number:|Not After :"
   chmod 644 $fn.crt $fn.key
 }
 
+gen_cert pm02.srv.dxpydk pm02.srv.dxpydk
 gen_cert pm02.otvl.org pm02.otvl.org
-gen_cert dxpydk dxpydk
