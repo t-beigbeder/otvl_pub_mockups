340cc51b36d601604015fa9828083cd85d82c2b8
thierry_otvl <tbeigbeder.nospam@gmail.com>
2024-06-26 07:29:59 +0200

    mc

--- m005_kubu_qs/guestbook/api/v1/guestbook_types.go
+++ m005_kubu_qs/guestbook/api/v1/guestbook_types.go
@@ -22,24 +22,40 @@ import (
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // GuestbookSpec defines the desired state of Guestbook
 type GuestbookSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Guestbook. Edit guestbook_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Quantity of instances
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=10
+	Size int32 `json:"size"`
+
+	// Name of the ConfigMap for GuestbookSpec's configuration
+	// +kubebuilder:validation:MaxLength=15
+	// +kubebuilder:validation:MinLength=1
+	ConfigMapName string `json:"configMapName"`
+
+	// +kubebuilder:validation:Enum=Phone;Address;Name
+	Type string `json:"alias,omitempty"`
 }
 
 // GuestbookStatus defines the observed state of Guestbook
 type GuestbookStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// PodName of the active Guestbook node.
+	Active string `json:"active"`
+
+	// PodNames of the standby Guestbook nodes.
+	Standby []string `json:"standby"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
 
 // Guestbook is the Schema for the guestbooks API
 type Guestbook struct {
